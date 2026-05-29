248a2b0b654d509191606af33c2a8ca66faac19f
svsxn <sebmagn102@gmail.com>
2026-05-29 23:37:47 +0200

    refactor: clean up

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,23 +1,18 @@
-import { ActivityIcon, ChevronsLeftIcon } from "lucide-react";
+import { ActivityIcon } from "lucide-react";
 import Nav from "./nav";
 
 export default function Sidebar() {
   return (
     <aside className="flex flex-col gap-6 w-64 border-r px-5 py-4">
-      <div className="flex justify-between items-center">
-        <div className="border p-2 rounded-sm border-primary/40">
-          <div className="relative">
-            <ActivityIcon size={24} />
-            <ActivityIcon
-              size={24}
-              color="var(--primary)"
-              className="absolute left-px bottom-px"
-            />
-          </div>
+      <div className="border p-2 rounded-sm border-primary/40 max-w-fit">
+        <div className="relative">
+          <ActivityIcon size={24} />
+          <ActivityIcon
+            size={24}
+            color="var(--primary)"
+            className="absolute left-px bottom-px"
+          />
         </div>
-        {/*<div className="bg-card px-0.5 py-1 border hover:cursor-pointer rounded-md">
-          <ChevronsLeftIcon size={20} />
-        </div>*/}
       </div>
       <Nav />
     </aside>
