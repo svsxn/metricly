84ceb09ce4fba6d98de5c6156f76bac51e68837d
svsxn <sebmagn102@gmail.com>
2026-05-29 18:45:08 +0200

    feat: add sidebar

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -1,3 +1,14 @@
+import { ActivityIcon, ChevronsLeftIcon } from "lucide-react";
+
 export default function Sidebar() {
-  return <aside className="w-64 border-r"></aside>;
+  return (
+    <aside className="w-64 border-r px-5 py-4">
+      <div className="flex justify-between items-center">
+        <ActivityIcon size={32} color="var(--primary)" />
+        <div className="bg-card px-0.5 py-1 border hover:cursor-pointer rounded-md">
+          <ChevronsLeftIcon size={20} />
+        </div>
+      </div>
+    </aside>
+  );
 }
