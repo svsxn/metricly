60d0f98869fcf54c2fc53e1bf093d9a735c8cfd8
svsxn <sebmagn102@gmail.com>
2026-05-29 19:11:23 +0200

    style: update colors

--- src/components/dashboard/card.tsx
+++ src/components/dashboard/card.tsx
@@ -2,7 +2,7 @@ import type { Card } from "@/types/general";
 
 export default function Card({ Icon, title, amount }: Card) {
   return (
-    <div className="flex flex-col gap-3 bg-card rounded-md px-4 py-3">
+    <div className="flex flex-col gap-3 bg-card rounded-md px-4 py-3 border">
       <div className="flex items-center gap-3">
         <div className="bg-background/30 p-1.5 rounded-md">
           <Icon />
