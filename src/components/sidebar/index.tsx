5891a577f673c3a2d516c8153d81ba47dd7abfcd
svsxn <sebmagn102@gmail.com>
2026-05-29 23:48:05 +0200

    style: change sidebar color

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -3,7 +3,7 @@ import Nav from "./nav";
 
 export default function Sidebar() {
   return (
-    <aside className="flex flex-col gap-6 w-64 border-r px-5 py-4">
+    <aside className="flex flex-col gap-6 w-64 border-r px-5 py-4 bg-card">
       <div className="border p-2 rounded-sm border-primary/40 max-w-fit">
         <div className="relative">
           <ActivityIcon size={24} />
