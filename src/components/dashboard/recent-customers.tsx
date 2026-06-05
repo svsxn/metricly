c0c81001801de9a09b3a9a490ec8a3f15f051d94
svsxn <sebmagn102@gmail.com>
2026-06-05 21:11:51 +0200

    feat: add button

--- src/components/dashboard/recent-customers.tsx
+++ src/components/dashboard/recent-customers.tsx
@@ -1,9 +1,20 @@
 import Card from "../card";
+import { Button } from "../ui/button";
 import CustomersTable from "./customers-table";
 
 export default function RecentCustomers() {
   return (
-    <Card header={{ title: "Recent Customers" }} className="col-span-4">
+    <Card
+      header={{
+        title: "Recent Customers",
+        action: (
+          <Button variant="outline" size="sm">
+            View all customers
+          </Button>
+        ),
+      }}
+      className="col-span-4"
+    >
       <CustomersTable />
     </Card>
   );
