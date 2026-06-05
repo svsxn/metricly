890d80abc1cc78fba4a700b75c3f1fc3c1a13e5f
svsxn <sebmagn102@gmail.com>
2026-06-05 23:28:58 +0200

    feat: change sidebar to sticky

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -3,7 +3,7 @@ import Nav from "./nav";
 
 export default function Sidebar() {
   return (
-    <aside className="hidden lg:flex flex-col gap-6 w-64 border-r px-5 py-4 bg-card">
+    <aside className="sticky top-0 h-svh shrink-0 hidden lg:flex flex-col gap-6 w-64 border-r px-5 py-4 bg-card">
       <div className="border p-2 rounded-sm border-primary/40 max-w-fit">
         <div className="relative">
           <ActivityIcon size={24} />
