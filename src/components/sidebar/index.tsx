f3936c41633253c2f1654196225eb5562d4ae4ac
svsxn <sebmagn102@gmail.com>
2026-06-06 22:25:56 +0200

    feat: add logo

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,18 +1,12 @@
-import { ActivityIcon } from "lucide-react";
 import Nav from "./nav";
+import Image from "next/image";
 
 export default function Sidebar() {
   return (
     <aside className="sticky top-0 h-svh shrink-0 hidden lg:flex flex-col gap-6 w-64 border-r px-5 py-4 bg-card">
-      <div className="border p-2 rounded-sm border-primary/40 max-w-fit">
-        <div className="relative">
-          <ActivityIcon size={24} />
-          <ActivityIcon
-            size={24}
-            color="var(--primary)"
-            className="absolute left-px bottom-px"
-          />
-        </div>
+      <div className="flex items-center gap-2 hover:cursor-pointer w-fit">
+        <Image src="/logo.svg" alt="Metricly" width={32} height={32} priority />
+        <span className="text-2xl font-medium">Metricly</span>
       </div>
       <Nav />
     </aside>
