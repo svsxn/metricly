aa2f8eab6b95b8a72610358c3617a9d62d4eedfb
svsxn <sebmagn102@gmail.com>
2026-06-08 17:33:45 +0200

    feat: add redirect

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,39 @@
+import Stat from "@/components/dashboard/stat";
+import Header from "@/components/dashboard/header";
+import RecentActivity from "@/components/dashboard/recent-activity";
+import { STATS } from "@/data/stats";
+import OverTime from "@/components/dashboard/over-time";
+import AcquisitionChannels from "@/components/dashboard/acquisition-channels";
+import RecentCustomers from "@/components/dashboard/recent-customers";
+
+export default function Page() {
+  return (
+    <div className="p-6">
+      <Header />
+      <div className="grid grid-cols-4 gap-4 mt-6">
+        {STATS.map((card, index) => (
+          <Stat
+            key={card.title}
+            index={index}
+            Icon={card.Icon}
+            title={card.title}
+            amount={card.amount}
+          />
+        ))}
+        <OverTime
+          title="Revenue Over Time"
+          color="var(--chart-1)"
+          seriesLabel="Revenue"
+        />
+        <OverTime
+          title="Active Users Over Time"
+          color="var(--chart-2)"
+          seriesLabel="Users"
+        />
+        <RecentActivity />
+        <AcquisitionChannels />
+        <RecentCustomers />
+      </div>
+    </div>
+  );
+}
