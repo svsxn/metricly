16667b86b9de899f8d577dfe7d9c7247d23fc597
svsxn <sebmagn102@gmail.com>
2026-06-10 22:32:40 +0200

    refactor: clean up

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,24 +2,6 @@ import { cn } from "@/lib/utils";
 import { LucideIcon } from "lucide-react";
 import type { ElementType, ReactNode } from "react";
 
-// type IconProps = {
-//   Icon: LucideIcon;
-//   color: string;
-//   background: string;
-// };
-
-// type HeaderProps = {
-//   title: string;
-//   icon?: IconProps;
-//   action?: React.ReactNode;
-// };
-
-// type Props = {
-//   header: HeaderProps;
-//   children: React.ReactNode;
-//   className?: string;
-// };
-
 function Card<TElement extends ElementType = "div">({
   as,
   children,
@@ -80,33 +62,5 @@ function CardTitle({ children }: { children: ReactNode }) {
 function CardAction({ children }: { children: ReactNode }) {
   return <div className="shrink-0">{children}</div>;
 }
-
-// function Card({ header, children, className }: Props) {
-//   return (
-//     <div
-//       className={cn(
-//         "flex flex-col gap-3 bg-card border rounded-sm p-4 sm:px-6 sm:py-5",
-//         className,
-//       )}
-//     >
-//       <div className="flex justify-between">
-//         {header.icon ? (
-//           <div className="flex items-center gap-3">
-//             <div className={cn("p-1.5 rounded-md", header.icon.background)}>
-//               <header.icon.Icon color={header.icon.color} />
-//             </div>
-//             <h2 className="text-sm md:text-base leading-tight">
-//               {header.title}
-//             </h2>
-//           </div>
-//         ) : (
-//           <h2>{header.title}</h2>
-//         )}
-//         {header.action && header.action}
-//       </div>
-//       {children}
-//     </div>
-//   );
-// }
 
 export { Card, CardHeader, CardTitle, CardAction };
