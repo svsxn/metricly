060206464179a0b60743c43fcf61f03f5464e5d6
svsxn <sebmagn102@gmail.com>
2026-06-14 00:08:48 +0200

    feat: add plan select

--- src/components/customers/toolbar.tsx
+++ src/components/customers/toolbar.tsx
@@ -5,11 +5,18 @@ import {
   InputGroupAddon,
   InputGroupInput,
 } from "../ui/input-group";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 export default function Toolbar() {
   return (
     <Card className="flex-row justify-between col-span-4">
-      <div>
+      <div className="flex gap-3">
         <InputGroup className="rounded-sm border-border bg-card!">
           <InputGroupInput
             placeholder="Search customers..."
@@ -19,6 +26,20 @@ export default function Toolbar() {
             <SearchIcon className="text-muted-foreground/50" />
           </InputGroupAddon>
         </InputGroup>
+        <Select defaultValue="all">
+          <SelectTrigger className="w-45 shrink-0 border-border bg-card! rounded-sm">
+            <div className="flex gap-2">
+              <span className="text-muted-foreground">Plan:</span>
+              <SelectValue placeholder="All plans" />
+            </div>
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="all">All plans</SelectItem>
+            <SelectItem value="free">Free</SelectItem>
+            <SelectItem value="pro">Pro</SelectItem>
+            <SelectItem value="business">Business</SelectItem>
+          </SelectContent>
+        </Select>
       </div>
       <div>Export</div>
     </Card>
