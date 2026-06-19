7a190f75c3d7040f7f7156ba234fea98d487f626
svsxn <sebmagn102@gmail.com>
2026-06-19 21:41:21 +0200

    feat: update over time charts select

--- src/components/dashboard/over-time-charts/period-select.tsx
+++ src/components/dashboard/over-time-charts/period-select.tsx
@@ -10,13 +10,26 @@ import {
 export default function PeriodSelect() {
   return (
     <Select defaultValue="monthly">
-      <SelectTrigger className="w-full max-w-24 rounded-sm border-border/70 text-xs">
+      <SelectTrigger className="w-24 rounded-sm border-border bg-card! text-xs">
         <SelectValue />
       </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectItem value="monthly">Monthly</SelectItem>
-          <SelectItem value="yearly">Yearly</SelectItem>
+      <SelectContent
+        position="popper"
+        className="min-w-0 w-(--radix-select-trigger-width) rounded-sm"
+      >
+        <SelectGroup className="space-y-1">
+          <SelectItem
+            value="monthly"
+            className="rounded-sm pr-2 text-xs data-[state=checked]:bg-accent data-[state=checked]:text-accent-foreground [&>span:first-child]:hidden"
+          >
+            Monthly
+          </SelectItem>
+          <SelectItem
+            value="yearly"
+            className="rounded-sm pr-2 text-xs data-[state=checked]:bg-accent data-[state=checked]:text-accent-foreground [&>span:first-child]:hidden"
+          >
+            Yearly
+          </SelectItem>
         </SelectGroup>
       </SelectContent>
     </Select>
