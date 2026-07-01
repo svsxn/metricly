60c5453c6c3d0c95e79c45704d87100974fc3ec5
svsxn <sebmagn102@gmail.com>
2026-07-01 16:33:54 +0200

    refactor: remove unused

--- src/components/dashboard/acquisition-channels/index.tsx
+++ src/components/dashboard/acquisition-channels/index.tsx
@@ -3,27 +3,6 @@ import { ArrowRightIcon } from "lucide-react";
 import ChartPie from "./chart-pie";
 import Legend from "./legend";
 
-const channels = [
-  {
-    name: "Direct",
-    users: "2,892",
-    percentage: "32.1%",
-    colorClassName: "bg-chart-1",
-  },
-  {
-    name: "Google Search",
-    users: "2,310",
-    percentage: "27.2%",
-    colorClassName: "bg-chart-2",
-  },
-  {
-    name: "Referral",
-    users: "1,640",
-    percentage: "19.3%",
-    colorClassName: "bg-chart-3",
-  },
-] as const;
-
 export default function AcquisitionChannels() {
   return (
     <Card className="col-span-4 sm:col-span-2">
